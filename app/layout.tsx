cf7186c71db6de052cc317f1feb0a11016f45119
Derin Owoade <85637610+derin-art@users.noreply.github.com>
2024-11-14 19:23:52 +0100

    small fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,9 @@ export default async function RootLayout({
     >
    
     <body className='text-black relative  '>
-    <Suspense>
+    
  <Navbar></Navbar>
-      </Suspense>
+   
    <ContextWrapper>
    <LenisHorizontalWrapper>
    {children}
