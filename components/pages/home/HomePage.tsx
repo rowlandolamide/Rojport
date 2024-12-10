9bd0674c19258516cdd8300190edb9ed05ff656b
Derin Owoade <85637610+derin-art@users.noreply.github.com>
2024-12-10 13:32:33 +0100

    performance: rendering both  Desktop and mobile components

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 import { useContext } from 'react';
 import { MainContextWrapperType, ContextMain } from '@/components/global/ContextWrapper';
 import MobileAndTabletHomeScreen from "./MobileAndTabletHomeScreen";
-/*  import HomePageHorizontal from './HomePageHorizontal'; */
+import HomePageHorizontal from './HomePageHorizontal'; 
 /* const HomePageHorizontal = dynamic(()=>import("./HomePageHorizontal"))  */
 import useMediaQuery from "@/components/hooks/useMediaQuery";
 
@@ -19,6 +19,7 @@ function HomePage(props: {data: HomePagePayload | null}) {
     const isLaptop = lenisCurrent ? x > 1279: false
     return (
         <div>
+            <div className='xl:hidden w-full'> <HomePageHorizontal data={props.data}></HomePageHorizontal></div>
             <MobileAndTabletHomeScreen data={props.data}></MobileAndTabletHomeScreen>
         {/*     {isLaptop ? <HomePageHorizontal data={props.data}></HomePageHorizontal> : <MobileAndTabletHomeScreen data={props.data}></MobileAndTabletHomeScreen>} */}
         </div>
