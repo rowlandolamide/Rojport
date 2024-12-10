885fe1f902abac35db6fd5f7cbe05a6d6c227138
Derin Owoade <85637610+derin-art@users.noreply.github.com>
2024-12-10 14:33:24 +0100

    clean up

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -16,12 +16,12 @@ import type { HomePagePayload } from "@/types";
 function HomePage(props: {data: HomePagePayload | null}) {
     const {lenisCurrent} = useContext(ContextMain) as MainContextWrapperType
     const {x} = useMediaQuery()
-    const isLaptop = lenisCurrent ? x > 1279: false
+   
     return (
         <div>
             <div className='hidden xl:block w-full'> <HomePageHorizontal data={props.data}></HomePageHorizontal></div>
             <MobileAndTabletHomeScreen data={props.data}></MobileAndTabletHomeScreen>
-        {/*     {isLaptop ? <HomePageHorizontal data={props.data}></HomePageHorizontal> : <MobileAndTabletHomeScreen data={props.data}></MobileAndTabletHomeScreen>} */}
+     
         </div>
     );
 }
