c1006efc75500a86ab3ccf8293f90c05dcc49d93
Derin Owoade <85637610+derin-art@users.noreply.github.com>
2025-08-17 15:43:44 +0100

    clean up

--- app/(personal)/projects/[slug]/page.tsx
+++ app/(personal)/projects/[slug]/page.tsx
@@ -86,6 +86,7 @@ export default async function ProjectSlugRoute({ params }: Props) {
               isProjectSummary
               title={initial.data?.title || ''}
               overview
+              tags={initial.data?.tag}
               year={initial.data?.year}
               disciplines={initial.data?.disci}
               body={initial.data?.overview}
